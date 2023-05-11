7478a1b53f1c0579fcf4dd86f2f27b4630e5a1a6
Arvind Suthar <arvindsuthar198@gmail.com>
2023-05-11 12:12:22 +0530

    i/p from user

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::io;
+
 fn main() {
     println!("Guess the number!");
 
     // thread_rng() gives us the particular random number generator
     // start..=end is inclusive on the lower and upper bounds
     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        // creates a new, empty string
+        let mut guess = String::new();
+        
+        // the stdin function returns an instance of std::io::Stdin,
+        // which is a type that represents a handle to the standard i/p.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+    }
 }
