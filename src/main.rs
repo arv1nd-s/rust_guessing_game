ead0f6e5504960051d78477696f6416a3bf27328
Arvind Suthar <arvindsuthar198@gmail.com>
2023-05-11 12:10:07 +0530

    secret number generated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    // thread_rng() gives us the particular random number generator
+    // start..=end is inclusive on the lower and upper bounds
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 }
